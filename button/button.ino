e5267cdcf76bf3e4e59d373706e5226228820dd1
Nevan Mukherjee <nm67037@uga.edu>
2024-11-09 20:14:13 -0500

    got button working, need to work on hall sensor logic

--- button/button.ino
+++ button/button.ino
@@ -1,12 +1,14 @@
 //this code was sourced from the Adafruit website
 
 int fsrAnalogPin = 5; 
+int Hall_Sensor_Pin = 10;
 int fsrReading;       
 int threshold = 600;  
 bool state = false;   
 bool running = false; 
 
 void setup() {
+  pinMode(Hall_Sensor_Pin, INPUT);
   Serial.begin(115200);
 }
 
@@ -30,7 +32,12 @@ void loop() {
   // Code that runs once "running" is true
   if (running) {
     Serial.println("Running part of the code...");
-    // Add your additional code here
+    
+    float hall_voltage;
+    hall_voltage = analogRead(Hall_Sensor_Pin);
+    Serial.println(hall_voltage);
+
+
     delay(1000); // Simulate processing time
   }
 }
