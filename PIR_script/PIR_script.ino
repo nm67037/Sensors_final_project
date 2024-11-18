ea0ea7718cb945d8df92a73c8711048003cfba1f
Nevan Mukherjee <nm67037@uga.edu>
2024-11-17 23:22:34 -0500

    push

--- PIR_script/PIR_script.ino
+++ PIR_script/PIR_script.ino
@@ -1,19 +1,39 @@
 #define pirPin 3
 
+//ultrasonic parameters:
+const int trig_pin = 4;
+const int echo_pin = 6;
+float timing = 0.0;
+float distance = 0.0;
+
 void setup() {
   Serial.begin(115200);
   pinMode(pirPin, INPUT);
+  pinMode(echo_pin, INPUT);
+  pinMode(trig_pin, OUTPUT);
+  digitalWrite(trig_pin, LOW);
 }
 
 void loop() {
   int pirState = digitalRead(pirPin);
-  Serial.println(pirState);
+  
 
-  if (pirState == HIGH) {
-  //  Serial.println("Motion detected!");
-  } else {
-    //Serial.println("No motion.");
-  }
+  digitalWrite(trig_pin, LOW);
+  delay(2);
+  
+  digitalWrite(trig_pin, HIGH);
+  delay(10);
+  digitalWrite(trig_pin, LOW);
+  
+  timing = pulseIn(echo_pin, HIGH);
+  distance = (timing * 0.034) / 2;
+  distance = constrain(distance, 0, 20);
+  
+  // Serial.print("Distance: ");
+  Serial.print(distance);Serial.print(",");
+  Serial.println(pirState);
+  //Serial.println("cm | ");
+
 
   delay(100); // Check every 100 milliseconds
 }
